de8380edf6ff368173eea074bc2ca1de9ac0628a
godwynnn <78883893+godwynnn@users.noreply.github.com>
2021-10-12 14:41:16 +0100

    Update settings.py

--- forex/settings.py
+++ forex/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,4 +152,4 @@ EMAIL_USE_TLS=''
 
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
